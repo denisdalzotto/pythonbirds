24791b8bc95d8cd21b8512407eee7c9b106033cf
DD <dalzottod@gmail.com>
2019-02-02 12:19:43 -0200

    criado atributo de classe olhos,

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,6 +1,8 @@
 class Pessoa:
+    olhos = 2 # criado atributo de classe
+
     def __init__(self, *filhos, nome=None, idade=33):
-        self.idade = idade
+        self.idade = idade         #são os objetos
         self.nome = nome
         self.filhos = list(filhos)
 
@@ -19,5 +21,12 @@ if __name__ == '__main__':
         print(filhos.nome)
     lucas.sobrenome = 'Dalzotto'
     del lucas.filhos
-    print(lucas.__dict__)
+    lucas.olhos = 1 # aqui criei um atributo na instância.
+    del lucas.olhos # foi deletado o atributo do objeto lucas a instância.
+    print(lucas.__dict__) # o dict só mostra os atributos de instâncias
     print(denis.__dict__)
+    Pessoa.olhos = 3 # foi criado um atributo de olhos na class todos tem 3.
+    print(Pessoa.olhos)
+    print(lucas.olhos)
+    print(denis.olhos)
+    print(id(Pessoa.olhos), id(lucas.olhos), id(denis.olhos))
